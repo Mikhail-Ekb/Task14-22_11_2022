b4c87c9b729b6b999ca54fa648306e8ccb793240
MIKHAIL <MIKHAIL@DESKTOP-6MU8OHN>
2022-11-23 22:44:30 +0500

    Добавьте файлы проекта.

---
+++ Task14-22_11_2022/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task14_22_11_2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Создание строковой переменной
+            "path" = C:\Users\MIKHAIL\Desktop\C#\ИТМО\Task13;
+
+            //получение файлов внутри каталога
+            string[] files = Directory.GetFiles(path, "*" SearchOption.AllDirectories);
+
+            //перборка
+            foreach (string file in files)
+            {
+                //Вывод
+                Console.WriteLine(;
+            }
+            Console.ReadKey();
+        }
+    }
+}
